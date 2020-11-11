8d48fa0a12bd826943b54fe65ff715288b3034b0
Frmnmaulanaa <frmanmaulanaa@gmail.com>
2020-11-11 16:39:51 +0700

    Semoga Benar

---
+++ Latihan02/Latihan.py
@@ -0,0 +1,13 @@
+a, b, c = input()
+if a > b:
+  if b > c:
+    maks = a
+  else:
+  if c > a:
+    maks = c
+  elif a > c:
+    maks = b
+  elif c > b:
+    maks = c
+
+print maks
---
+++ Latihan03/Tugas7.py
@@ -0,0 +1,8 @@
+ketik = 10
+jumlah = (ketik)
+for i in range(ketik):
+        for c in range(jumlah):
+                tab = i + c
+                print("{0:>5}".format (tab), end ="")
+        print("")
+        
